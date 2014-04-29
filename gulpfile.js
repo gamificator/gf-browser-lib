a699724d2487373b76aa0868ac93efdd3e933388
Yury Chebotaev <yury.79120345101@gmail.com>
2014-04-29 15:55:14 +0600

    changes ...

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,6 @@ gulp.task('default',['build']);
 
 gulp.task('build');
 
-gulp.task('pages', ['build'], function(done){
-	var exec = require('child_process').exec;
-	
-	exec('git push github gh-pages', function(err, stdout, stderr) {
-		return done(err);
-	});
-	
-});
-
 gulp.once('stop', function() {
 	process.exit(0);
 });
